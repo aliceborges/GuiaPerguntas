84aea5811e83664a5c4f8935157b06ba9eed192c
Alice Borges dos Santos <aliceborges@outlook.com.br>
2021-06-18 20:21:24 -0300

    estruturas condicionais

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ app.set('view engine', 'ejs')
 app.get('/:nome/:lang' , (req , res)=>{
     var nome = req.params.nome
     var lang = req.params.lang
+    var exibirMsg = false
+
     res.render("index", 
     {
         nome: nome, 
         lang: lang,
         empresa: "Guia do Programador",
-        inscritos: 8000
+        inscritos: 8000,
+        msg: exibirMsg
     })
 })
 
